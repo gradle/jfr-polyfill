b507bfa1d3c235c7202d44803dd6911b99e04455
Leonard Brünings <leonard.bruenings@gradle.com>
2021-11-09 17:17:12 +0100

    Add Gradle Enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,13 @@
-rootProject.name = "jfr-polyfill-parent"
-include("jfr-polyfill")
-include("specs")
+plugins {
+    id("com.gradle.enterprise") version "3.7.1"
+}
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
 }
+
+rootProject.name = "jfr-polyfill-parent"
+include("jfr-polyfill")
+include("specs")
