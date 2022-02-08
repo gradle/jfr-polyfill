4c3a0227e4b54ba03108479211a731e84cf330c9
Leonard Brünings <leonard.bruenings@gradle.com>
2022-02-08 14:53:50 +0100

    Add publishing plugins and configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,26 +23,19 @@
  * or visit www.gradle.org if you need additional information or have any
  * questions.
  */
-pluginManagement {
-    plugins {
-        id("com.gradle.enterprise") version "3.8.1"
-        id("com.gradle.enterprise.gradle-enterprise-conventions-plugin") version "0.7.4"
-        id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
-    }
-}
-
 
 plugins {
-    id("com.gradle.enterprise")
-    id("com.gradle.enterprise.gradle-enterprise-conventions-plugin")
+    id("io.github.gradle-nexus.publish-plugin")
 }
 
-dependencyResolutionManagement {
+
+nexusPublishing {
+    packageGroup.set("org.gradle")
     repositories {
-        mavenCentral()
+        sonatype {
+            // https://central.sonatype.org/news/20210223_new-users-on-s01/
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
     }
 }
-
-rootProject.name = "jfr-polyfill-parent"
-include("jfr-polyfill")
-include("specs")
--- jfr-polyfill/build.gradle.kts
+++ jfr-polyfill/build.gradle.kts
@@ -26,10 +26,49 @@
 
 plugins {
     `java-library`
+    `maven-publish`
+    signing
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(8))
     }
+}
+
+
+publishing {
+    publications.create<MavenPublication>("mavenJava") {
+        artifactId = project.name
+        from(components["java"])
+
+        pom {
+            name.set("JFR Polyfill")
+            description.set("The jfr-polyfill library is a no-op polyfill implementation of the jdk.jfr.* interfaces and classes. The goal is to enable projects to add JFR events to their codebase without dropping support for old JDKs that don’t support JFR.")
+            url.set("https://github.com/gradle/jfr-polyfill/")
+            licenses {
+                license {
+                    name.set("Apache-2.0")
+                    url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                }
+            }
+            developers {
+                developer {
+                    name.set("The Gradle team")
+                    organization.set("Gradle Inc.")
+                    organizationUrl.set("https://gradle.com")
+                }
+            }
+            scm {
+                connection.set("scm:git:git://github.com/gradle/jfr-polyfill.git")
+                developerConnection.set("scm:git:ssh://git@github.com:gradle/jfr-polyfill.git")
+                url.set("https://github.com/gradle/jfr-polyfill/")
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+    useInMemoryPgpKeys(System.getenv("PGP_SIGNING_KEY"), System.getenv("PGP_SIGNING_KEY_PASSPHRASE"))
 }
