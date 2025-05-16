f94337067de6880416085865fff83f1de5829d8c
bot-gh-dv-shared <bot-gh-dv-shared+renovate@gradle.com>
2025-05-15 19:46:25 -0500

    [Renovate Bot] Update plugin io.github.gradle.gradle-enterprise-conventions-plugin to v0.10.3 (#45)
    Signed-off-by: Renovate Bot for Gradle <bot-gh-dv-shared+renovate@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@
 pluginManagement {
     plugins {
         id("com.gradle.develocity") version "4.0.1"
-        id("io.github.gradle.gradle-enterprise-conventions-plugin") version "0.10.2"
+        id("io.github.gradle.gradle-enterprise-conventions-plugin") version "0.10.3"
         id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     }
 }
