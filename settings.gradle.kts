75883948366cfe408d33611d1677c35fa6612b3a
bot-gh-dv-shared <bot-gh-dv-shared+renovate@gradle.com>
2025-07-15 09:26:31 -0500

    [Renovate Bot] Update plugin com.gradle.develocity to v4.1 (#51)
    Signed-off-by: Renovate Bot for Gradle <bot-gh-dv-shared+renovate@gradle.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,7 +25,7 @@
  */
 pluginManagement {
     plugins {
-        id("com.gradle.develocity") version "4.0.2"
+        id("com.gradle.develocity") version "4.1"
         id("io.github.gradle.gradle-enterprise-conventions-plugin") version "0.10.3"
         id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
     }
